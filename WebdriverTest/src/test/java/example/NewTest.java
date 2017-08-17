630403a106b8574f8e5eed3d3411e6e6e41af69c
Gate6-70\Owner <manish.dangas@gate6.com>
2017-08-17 16:27:04 +0530

    test

---
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -0,0 +1,35 @@
+package example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class NewTest {
+	public String st = System.setProperty("webdriver.chrome.driver", "ChromeDriver/chromedriver.exe");
+
+	public WebDriver driver = new ChromeDriver();
+
+
+	@Test
+	public void testEasy() {
+
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Demo Guru99 Page"));
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		driver.get("http://demo.guru99.com/selenium/guru99home/");
+
+	}
+
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+}
