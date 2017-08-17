ae5b40845acb3dc6eff3fe55a9c9532cad8c5f32
Gate6-70\Owner <manish.dangas@gate6.com>
2017-08-17 15:46:23 +0530

    test

--- WebdriverTestNew/src/test/java/example/NewTest.java
+++ WebdriverTestNew/src/test/java/example/NewTest.java
@@ -11,7 +11,7 @@ import org.testng.annotations.AfterTest;
 
 public class NewTest {
 public String st = System.setProperty("webdriver.chrome.driver",
-            "/ChromeNewDriver/chromedriver");
+            "/ChromeNewDriver/chromedriverWin.exe");
 
 	public WebDriver driver = new ChromeDriver();
 
